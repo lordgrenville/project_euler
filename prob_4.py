2c326f78aeb0d7ad7c6e4c76bc805a50407a2185
Josh Friedlander <joebobson87@yahoo.com>
2018-08-20 13:34:36 +0300

    fourth problem - oneliner

--- prob_4.py
+++ prob_4.py
@@ -4,8 +4,11 @@ def is_palindromic(input):
 assert not is_palindromic('cat')
 assert is_palindromic('ablewasiereisawelba')
 
+# oneliner:
+print (max([i* j for i in range(100,1000) for j in range(100,1000) if is_palindromic(i * j)]))
+
+# namely...:
 palindromes = []
-i, j = 999, 999
 for i in range(1000):
     for j in range(1000):
         if is_palindromic(i * j):
