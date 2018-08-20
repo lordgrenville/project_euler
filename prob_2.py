fae06a13d2a3d1ec7d400306563f865122e5124b
Josh Friedlander <joebobson87@yahoo.com>
2018-08-20 11:21:11 +0300

    second problem

---
+++ prob_2.py
@@ -0,0 +1,11 @@
+def fibonacci(limit):
+    old, new = 1,1
+    sequence = set()
+    while new < limit:
+        sequence.update([old, new])
+        new_term = old + new
+        old = new
+        new = new_term
+    return sequence
+	
+sum([i for i in fibonacci(4000000) if i%2 == 0])
