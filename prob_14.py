af9cda5730e14418bef936182dbc7ffdb085473c
Josh Friedlander <joebobson87@yahoo.com>
2018-09-06 13:30:36 +0300

    minor fixes

--- prob_14.py
+++ prob_14.py
@@ -1,19 +1,17 @@
 collatz = lambda x: x // 2 if x % 2 == 0 else 3 * x + 1
 
 
-def chainer(x):
-    chain = 1
-    while x != 1:
-        x = collatz(x)
-        chain += 1
-    return chain
+def chainer(x, chain):
+    if x == 1:
+        return chain
+    chain += 1
+    return chainer(collatz(x), chain)
 
 
 winner, longest = 1, 1
 for x in range(2, 1000001):
-    if chainer(x) > longest:
-        longest = chainer(x)
+    if chainer(x, 1) > longest:
+        longest = chainer(x, 1)
         winner = x
 
 print(winner)
-
