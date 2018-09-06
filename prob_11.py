8dae1d837a95a644e84a44e61b2d7a94ffa8432c
Josh Friedlander <joebobson87@yahoo.com>
2018-09-06 13:28:30 +0300

    fixes prob 11, adds recursion in 14, minor fixes

--- prob_11.py
+++ prob_11.py
@@ -1,4 +1,5 @@
-texts = '''08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08
+texts = '''
+08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08
 49 49 99 40 17 81 18 57 60 87 17 40 98 43 69 48 04 56 62 00
 81 49 31 73 55 79 14 29 93 71 40 67 53 88 30 03 49 13 36 65
 52 70 95 23 04 60 11 42 69 24 68 56 01 32 56 71 37 02 36 91
@@ -30,7 +31,7 @@ def get_prod(multipliable_list):
 window = 4
 numbers, horizontals, verticals, diagonals, vertical_tops = [], [], [], [], []
 
-for line in texts.split('\n'):
+for line in texts.split('\n')[1:]:
     numbers.append([int(i) for i in line.split(' ')])
 
 for line in numbers:
@@ -44,10 +45,13 @@ for line in verticals:
     vertical_tops.append(max([get_prod(line[a:a + window]) for a in range(len(line) - window + 1)]))
 v_max = max(vertical_tops)
 
-for i in range(len(numbers) - window + 1):
-    for j in range(len(numbers) - window + 1):
+for i in range(len(numbers) - window):
+    for j in range(len(numbers) - window):
         diagonals.append(get_prod([numbers[i + k][j + k] for k in range(window)]))
-        diagonals.append(get_prod([numbers[i + k][j + k] for k in range(window)]))
+
+for i in range(window - 1, len(numbers)):
+    for j in range(len(numbers) - window):
+        diagonals.append(get_prod([numbers[i - k][j + k] for k in range(window)]))
 d_max = max(diagonals)
 
 print(max(v_max, h_max, d_max))
