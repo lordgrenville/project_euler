92e13fa9e06423e5c20abdc8060359256743a068
Josh Friedlander <joebobson87@yahoo.com>
2018-09-23 15:28:13 +0300

    prob 10 in ~30s with reusable sieve of eratosthenes

--- prob_10.py
+++ prob_10.py
@@ -1,13 +1,20 @@
 import time
-from prob_7 import primality_tester
+
+
+def sieve_of_eratosthenes(n):
+    naive = set(range(3, n, 2)).union({2})
+    primes, non_primes = [], []
+    for i in range(2, int(n**0.5) + 1):
+        if i not in non_primes:
+            primes.append(i)
+            for j in range(i*i, n+1, i):
+                non_primes.append(j)
+    return naive.difference(non_primes)
+
+
 start = time.time()
-prime_list = [2]
-num = 3
-while num < 2000001:
-    if primality_tester(prime_list, num):
-        prime_list.append(num)
-    print(num)
-    num += 2
-print(sum(prime_list))
+print(sum(sieve_of_eratosthenes(2000000)))
 end = time.time()
 print('total running time is ', end - start)
+
+assert sieve_of_eratosthenes(10) == {2, 3, 5, 7}
