cc83a7ef715cc2024f8fbe17b8d4ebbf79d55847
lordgrenville <16547083+lordgrenville@users.noreply.github.com>
2019-06-17 14:55:35 +0300

    improves prob 49

--- prob_49.py
+++ prob_49.py
@@ -1,8 +1,6 @@
-from prob_10 import sieve_of_eratosthenes as sieve
 from itertools import combinations
+from prob_10 import sieve_of_eratosthenes
 
 same_dig = lambda x,y,z: sorted(str(x)) == sorted(str(y)) == sorted(str(z))
 same_dist = lambda x,y,z: x - y == y - z
-
-
-print([j for j in [i for i in combinations([k for k in sieve(10000) if k > 1000], r=3) if same_dist(*i)] if same_dig(*j)])
+print([i for i in combinations([k for k in sieve_of_eratosthenes(10000) if k > 1000], r=3) if same_dist(*i) and same_dig(*i)])
