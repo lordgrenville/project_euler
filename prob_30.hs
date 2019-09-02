4c2f054bc6618ed3e313e5d7abb78ab04dc03fcb
Josh Friedlander <joebobson87@yahoo.com>
2019-09-02 22:36:02 +0300

    change prob_30 haskell to be compilable

--- prob_30.hs
+++ prob_30.hs
@@ -3,4 +3,6 @@ toDigits 0 = []
 toDigits x = toDigits (x `div` 10) ++ [x `mod` 10]
 
 isSumofFifth n = n == sum (map (^5) (toDigits n))
-sum (filter isSumofFifth [9..9999999])
+
+main = putStrLn (show ans) 
+ans = sum (filter isSumofFifth [9..9999999])
