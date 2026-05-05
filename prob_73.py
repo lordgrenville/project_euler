c4195f84fa4e70954cc0a808585782d2756f100b
Josh Friedlander <16547083+lordgrenville@users.noreply.github.com>
2026-05-05 23:37:17 +0300

    problem 73

---
+++ prob_73.py
@@ -0,0 +1,7 @@
+from fractions import Fraction
+from itertools import combinations
+
+# naive solution: all possibilities, get unique, sort, count the difference
+x = sorted(set(Fraction(*x) for x in (combinations(range(1, 12001), 2))))
+
+print(x.index(Fraction(1, 2)) - x.index(Fraction(1, 3)) - 1)
