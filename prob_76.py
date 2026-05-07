8fc3c435804b205e4c824fed9f805572e0c261f5
Josh Friedlander <16547083+lordgrenville@users.noreply.github.com>
2026-05-07 16:27:11 +0300

    problem 76

---
+++ prob_76.py
@@ -0,0 +1,3 @@
+# Asked Wolfram Alpha for the integer partitions of 100: https://www.wolframalpha.com/input?i=Partitions+of+100
+# Removed 1 for the trivial case of 100 = 100 (since the question specifies sets of 2 or more summands)
+# TODO: solve this algorithmically. prob 31 is the basis for it, it is change-making when you have a coin of every natural number
